523ea81272b5ea3a3297a96635b76e963a5d7194
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2023-12-06 20:09:57 +0800

    实现http.Handler接口

--- main.go
+++ main.go
@@ -6,19 +6,25 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/hello", helloHandler)
-	log.Fatal(http.ListenAndServe("0.0.0.0:80", nil))
+type Engine struct{}
 
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-}
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	for k, v := range r.Header {
-		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	case "/hello":
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", r.URL)
 	}
+}
+
+func main() {
+	engine := new(Engine)
+	// 在 Go 语言中，实现了接口方法的 struct 都可以强制转换为接口类型。
+	log.Fatal(http.ListenAndServe("0.0.0.0:80", engine))
+
 }
